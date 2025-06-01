b61c74caaea99de2d7a54b5f2b7c0b41a4f3116c
abhishek hiwarkar <abhiwarkar123@gmail.com>
2025-06-01 13:57:35 +0530

    🔧 Configure Next.js settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+  },
+  // For running React Router DOM in Next.js environment
+  webpack: (config) => {
+    return config;
+  },
 };
 
 export default nextConfig;
